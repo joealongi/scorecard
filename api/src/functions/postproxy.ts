9b209ae0c6b7f43338f66a3678b6c608428c3bb8
Joe Alongi <contact@joealongi.dev>
2025-07-01 20:58:33 -0400

    Test Functions Proxy

---
+++ api/src/functions/postproxy.ts
@@ -0,0 +1,48 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+
+import axios from "axios";
+
+const postrequest = async (
+  baseurl: string,
+  endpoint: string,
+  body: Object<any>
+) => {
+  try {
+    const { data } = await axios.post(baseurl + endpoint, body, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log("Error posting request in Azure", error);
+    return null;
+  }
+};
+
+export async function postproxy(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  try {
+    const baseurl = request.json.get("baseurl");
+    const endpoint = request.json.get("endpoint");
+    const body = request.json.get("body");
+    const data = await postrequest(baseurl, endpoint, body);
+    return data;
+  } catch (error) {
+    console.log("Error proxying posting request in Azure", error);
+    return null;
+  }
+}
+
+app.http("postproxy", {
+  methods: ["POST"],
+  authLevel: "anonymous",
+  handler: postproxy,
+});
--- web-app/src/client/RequestClient.ts
+++ web-app/src/client/RequestClient.ts
@@ -1,14 +1,34 @@
 import axios from "axios";
 
+// export const postRequest = async (endpoint: string, payloadExt: unknown) => {
+//   try {
+//     const body = new URLSearchParams(payloadExt as Record<string, string>);
+//     const baseurl = import.meta.env.VITE_BASE_API_URL ?? "";
+//     const { data } = await axios.post(baseurl + endpoint, body, {
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     console.log("Error posting request", error);
+//     throw error;
+//   }
+// };
+
 export const postRequest = async (endpoint: string, payloadExt: unknown) => {
   try {
     const body = new URLSearchParams(payloadExt as Record<string, string>);
-    const baseurl = import.meta.env.VITE_BASE_API_URL ?? "";
-    const { data } = await axios.post(baseurl + endpoint, body, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
+    const baseurl = import.meta.env.VITE_AZURE_API_URL ?? "";
+    const { data } = await axios.post(
+      baseurl + endpoint,
+      { baseurl, endpoint, body },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return data;
   } catch (error) {
     console.log("Error posting request", error);
