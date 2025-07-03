0b3cd50e369b4878a9665b29dcdeee8bf41fb76c
Joe Alongi <contact@joealongi.dev>
2025-07-02 23:35:20 -0400

    Add CORS

--- pinpointscorecourse/index.ts
+++ pinpointscorecourse/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import { post } from "./api/proxy";
 
@@ -7,33 +8,45 @@ import { post } from "./api/proxy";
 dotenv.config();
 const app = express();
 
+// Add CORS middleware to allow requests from the specified origin
+const corsOptions = {
+  origin: "https://pinpointscore.golf",
+  preflightContinue: false,
+  methods: "GET,POST,OPTIONS",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 // Add JSON body parsing middleware
 app.use(express.json());
 
 const PORT = process.env.PORT ?? 8080;
 
-app.get("/", (request: Request, response: Response) => {
+app.get("/", cors(corsOptions), (request: Request, response: Response) => {
   response.status(200).send("Hello World");
 });
 
-app.post("/proxy", async (request: Request<any>, response: Response<any>) => {
-  try {
-    console.log("Received request to proxy:", request.body);
-    const baseurl = request?.body?.baseurl;
-    const endpoint = request?.body?.endpoint;
-    const body = request?.body?.body;
-    console.log("Received request to proxy:", {
-      baseurl,
-      endpoint,
-      body,
-    });
-    const resp = await post(baseurl, endpoint, body);
-    response.status(200).send({ resp });
-  } catch (error) {
-    console.log("Error proxying post request", error);
-    response.status(400).send({ error: "Not" });
+app.post(
+  "/proxy",
+  cors(corsOptions),
+  async (request: Request<any>, response: Response<any>) => {
+    try {
+      console.log("Received request to proxy:", request.body);
+      const baseurl = request?.body?.baseurl;
+      const endpoint = request?.body?.endpoint;
+      const body = request?.body?.body;
+      console.log("Received request to proxy:", {
+        baseurl,
+        endpoint,
+        body,
+      });
+      const resp = await post(baseurl, endpoint, body);
+      response.status(200).send({ resp });
+    } catch (error) {
+      console.log("Error proxying post request", error);
+      response.status(400).send({ error: "Not" });
+    }
   }
-});
+);
 
 app
   .listen(PORT, () => {
