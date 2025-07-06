3f2b2a500b605f8a1bc7bc60b2025e60dd273d0c
Joe Alongi <contact@joealongi.dev>
2025-07-05 20:38:11 -0400

    Change configuration

--- pinpointscore/src/App.tsx
+++ pinpointscore/src/App.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./AppRoutes";
 
@@ -6,15 +8,17 @@ import Copyright from "./components/Copyright";
 
 function App() {
   return (
-    <BrowserRouter>
-      <main id="main" className="z-0 max-w-6xl m-9 xl:mx-auto">
-        <div className="flex flex-col flex-auto min-w-0 mt-3 px-3 md:px-0">
-          <Navbar />
-          <AppRoutes />
-          <Copyright />
-        </div>
-      </main>
-    </BrowserRouter>
+    <React.Fragment>
+      <BrowserRouter>
+        <main id="main" className="z-0 max-w-6xl m-9 xl:mx-auto">
+          <div className="flex flex-col flex-auto min-w-0 mt-3 px-3 md:px-0">
+            <Navbar />
+            <AppRoutes />
+            <Copyright />
+          </div>
+        </main>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
