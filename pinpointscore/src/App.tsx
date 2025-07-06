5587839aa2116332985e4c6fe521ac45cfe3d0f5
Joe Alongi <contact@joealongi.dev>
2025-07-05 20:30:13 -0400

    Cleanup browser router

--- pinpointscore/src/App.tsx
+++ pinpointscore/src/App.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./AppRoutes";
 
@@ -8,17 +6,15 @@ import Copyright from "./components/Copyright";
 
 function App() {
   return (
-    <React.Fragment>
-      <BrowserRouter>
-        <main id="main" className="z-0 max-w-6xl m-9 xl:mx-auto">
-          <div className="flex flex-col flex-auto min-w-0 mt-3 px-3 md:px-0">
-            <Navbar />
-            <AppRoutes />
-            <Copyright />
-          </div>
-        </main>
-      </BrowserRouter>
-    </React.Fragment>
+    <BrowserRouter>
+      <main id="main" className="z-0 max-w-6xl m-9 xl:mx-auto">
+        <div className="flex flex-col flex-auto min-w-0 mt-3 px-3 md:px-0">
+          <Navbar />
+          <AppRoutes />
+          <Copyright />
+        </div>
+      </main>
+    </BrowserRouter>
   );
 }
 
