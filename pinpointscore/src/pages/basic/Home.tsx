f463ea2ae231f4c9903be0dcfb0da5902d659940
Joe Alongi <contact@joealongi.dev>
2025-07-06 22:59:43 -0400

    Add Color

--- pinpointscore/src/pages/basic/Home.tsx
+++ pinpointscore/src/pages/basic/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <section>
         {/* Todo: Make this a component */}
         <h1 className="mx-auto text-3xl md:text-6xl font-bold subpixel-antialiased text-neutral-300 text-neutral-300">
-          Pinpointscore
+          <span className="text-lime-600">P</span>inpointscore
         </h1>
         {/* Todo: Make this a component */}
         <h3 className="my-3 md:my-9 width-half text-3xl md:text-6xl font-light subpixel-antialiased">
