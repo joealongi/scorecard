a3402d3ac75e4e1652102abde1204b6ea86cd88f
Joe Alongi <contact@joealongi.dev>
2025-09-21 15:24:02 -0400

    Fix prefix

--- course/utils/environment.ts
+++ course/utils/environment.ts
@@ -1,15 +1,8 @@
 export const environment = async (variable: any): Promise<any> => {
   try {
-    console.log("Environment Variables:");
-    for (const key in process.env) {
-      // You might want to filter out system-level environment variables
-      // if you only want to see those loaded from your .env file.
-      // For simplicity, this example lists all.
-      console.log(`${key}: ${process.env[key]}`);
-    }
     // Check for variable in both standard and prefixed formats
     const env = process.env?.[variable] ?? "";
-    const app = process.env?.[`APP_SETTINGS_${variable}`] ?? "";
+    const app = process.env?.[`APPSETTING_${variable}`] ?? "";
     // Return the found variable or null
     if (env) return env;
     if (app) return app;
