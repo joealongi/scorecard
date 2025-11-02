1f2b8fea0813010a6ab1b9b8045a74ae40fd2ace
Joe Alongi <contact@joealongi.dev>
2025-11-01 22:56:34 -0400

    Cleanup homepage

--- teebox/src/pages/HomePage.tsx
+++ teebox/src/pages/HomePage.tsx
@@ -1,18 +1,9 @@
 import * as React from "react";
 
-import { AuthenticationContext } from "../context/AuthenticationProvider";
-
 import HeadingOneComponent from "../components/HeadingOneComponent";
 import IntroductionComponent from "../components/IntroductionComponent";
 
 export default function HomePage() {
-  const { user } = React.useContext(AuthenticationContext);
-
-  React.useEffect(() => {
-    console.log("HomePage user after load", user);
-    return () => {};
-  }, []);
-
   return (
     <React.Fragment>
       <section>
