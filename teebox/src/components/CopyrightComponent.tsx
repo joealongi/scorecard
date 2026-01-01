9b0d00acfe05c18344b00567a975cb8d74cd33f0
Joe Alongi <contact@joealongi.dev>
2026-01-01 00:30:25 -0500

    Switch to pinpointscore links

--- teebox/src/components/CopyrightComponent.tsx
+++ teebox/src/components/CopyrightComponent.tsx
@@ -6,10 +6,7 @@ export default function CopyrightComponent() {
     <React.Fragment>
       <footer className="relative block mt-[9vh] text-xs subpixel-antialiased font-light text-left uppercase text-neutral-300">
         <p className="flex flex-col md:flex-row flex-auto my-3">
-          <span>
-            © {new Date()?.getFullYear()} Availing Inc. All rights
-            reserved.{" "}
-          </span>
+          <span>© {new Date()?.getFullYear()} Pinpointscore </span>
           <span className="mx-3 invisible md:visible">|</span>
           <span>
             <Link
@@ -26,32 +23,10 @@ export default function CopyrightComponent() {
             <Link
               className="hover:text-neutral-600 transition-all"
               rel="noopener noreferrer"
-              target="_blank"
-              to="https://availing.io/privacy"
-            >
-              Privacy policy
-            </Link>
-          </span>
-          <span className="mx-3 invisible md:visible">|</span>
-          <span>
-            <Link
-              className="hover:text-neutral-600 transition-all"
-              rel="noopener noreferrer"
-              target="_blank"
-              to="https://status.availing.io/"
-            >
-              Service monitor
-            </Link>
-          </span>
-          <span className="mx-3 invisible md:visible">|</span>
-          <span>
-            <Link
-              className="hover:text-neutral-600 transition-all"
-              rel="noopener noreferrer"
               target="_self"
-              to="mailto:contact@availing.io"
+              to="mailto:contact@pinpointscore.golf"
             >
-              contact@availing.io
+              contact@pinpointscore.golf
             </Link>
           </span>
         </p>
