5ad842cd4c4af70ff3b990ab260a984a5c416d41
Joe Alongi <contact@joealongi.dev>
2026-02-15 15:46:50 -0500

    Update footer

--- teebox/src/components/CopyrightComponent.tsx
+++ teebox/src/components/CopyrightComponent.tsx
@@ -6,7 +6,19 @@ export default function CopyrightComponent() {
     <React.Fragment>
       <footer className="relative block mt-[9vh] text-xs subpixel-antialiased font-light text-left uppercase text-neutral-300">
         <p className="flex flex-col md:flex-row flex-auto my-3">
-          <span>© {new Date()?.getFullYear()} Pinpointscore </span>
+          <span>© {new Date()?.getFullYear()} Pinpointscore</span>
+          <span className="mx-3 invisible md:visible">|</span>
+          <span>
+            Crafted by{" "}
+            <a
+              href="https://joealongi.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-neutral-600 transition-all"
+            >
+              Joe Alongi
+            </a>
+          </span>
           <span className="mx-3 invisible md:visible">|</span>
           <span>
             <Link
@@ -28,6 +40,17 @@ export default function CopyrightComponent() {
             >
               contact@pinpointscore.golf
             </Link>
+          </span>
+          <span className="mx-3 invisible md:visible">|</span>
+          <span>
+            <a
+              href="https://railway.com?referralCode=BpTk0g"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-neutral-600 transition-all"
+            >
+              Powered by Railway
+            </a>
           </span>
         </p>
       </footer>
